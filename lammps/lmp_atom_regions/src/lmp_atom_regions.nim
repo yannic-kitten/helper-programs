93dac73897afcd676d8899db02fcb97623d55d67
Yannic Kitten <y.kitten@fz-juelich.de>
2025-09-26 11:34:42 +0200

    Quickfix to allow generation according to arbitrary cut orders, required to test ALL_Histogram's new functionality for arbitrary cut order

--- lammps/lmp_atom_regions/src/lmp_atom_regions.nim
+++ lammps/lmp_atom_regions/src/lmp_atom_regions.nim
@@ -1,15 +1,39 @@
-import std / [cmdline, strformat, strutils, sequtils, math]
+import std / [cmdline, strformat, strutils, sequtils, unicode, math]
 import general_utils / error
 import basic_types, grids
+
+# NOTE: Quickfix arbitrary-cut-order
+#   To not spend too much time on integrating an arbitrary cut order into the Grid object structure,
+#   all grids remain to be calculated in historic cut order (e.g. zyx for staggered grid).
+#   Only after these region borders have benn calculated and can easily accessed by xyz, they are
+#   shuffled according to the intended cut order.
+#   This is done using the following translation procedure and dimorder attribute of System.
+proc changeDimOrder(origBorder: RegionBorder; dimorder: string): RegionBorder =
+  if dimorder in ["xyz", "xzy", "yxz", "yzx", "zxy", "zyx"]:
+    for i, d in dimorder:
+      let brdr = case i.range[:0..2]:
+                  of 0: origBorder.x
+                  of 1: origBorder.y
+                  of 2: origBorder.z
+      case d:
+        of 'x': result.x = brdr
+        of 'y': result.y = brdr
+        of 'z': result.z = brdr
+        else: discard
+  elif dimorder == "default":
+    return origBorder
+  else:
+    error fmt"invalid dim order '{dimorder}'"
 
 type System = object
   grid: Grid
   gap: float
   gapped_regions: seq[Region]
   atomparams: AtomParams
+  dimorder: string  # quickfix (eventually include into grid (as cutorder))
 
-proc initSystem(grid: Grid; gap: float; atomparams: AtomParams): System =
-  result = System(grid: grid, gap: gap, atomparams: atomparams)
+proc initSystem(grid: Grid; gap: float; atomparams: AtomParams; dimorder: string): System =
+  result = System(grid: grid, gap: gap, atomparams: atomparams, dimorder: dimorder)
   result.gapped_regions = result.grid.getRegions.mapIt(it.add_gap(gap))
 
 proc parseSystem(args: seq[string]): System =
@@ -21,22 +45,27 @@ proc parseSystem(args: seq[string]): System =
     let len = args[4..6].parseFloatTriple
     let region_gap = args[7].parseFloat
     let atomparams = args[8].split(',').parseAtomParams
-    let input_cuts = args[9].split(',').map(parseFloat)
+    let dimorder = case gridstyle:
+      of TensorGridStyle:    args[9].split(',')[0].reversed
+      of StaggeredGridStyle: args[9].split(',')[0].reversed
+      of TiledGridStyle:     "default"
+    let input_cuts = args[9].split(',')[1..^1].map(parseFloat)
     error_if(case gridstyle:
       of TensorGridStyle:    input_cuts.len != (dim.z + 1) + (dim.y + 1) + (dim.x + 1)
       of StaggeredGridStyle: input_cuts.len != (dim.z + 1) + dim.z * (dim.y + 1) + dim.z * dim.y * (dim.x + 1)
       of TiledGridStyle:     input_cuts.len != 2 * (dim.prod() - 1)
       , "The number of cuts does not fit the grid and processor dimensions")
-    return initSystem(newGrid(gridstyle, dim, len, input_cuts), region_gap, atomparams)
+    return initSystem(newGrid(gridstyle, dim, len, input_cuts), region_gap, atomparams, dimorder)
   except:
     error "invalid argument"
 
 proc lmp_region_cmds(sys: System): string =
   for reg in sys.gapped_regions:
+    let borders = reg.border.changeDimOrder(sys.dimorder)
     result &= fmt"region {reg.name} block " &
-              fmt"{reg.border.x.low:.6f} {reg.border.x.high:.6f} " &
-              fmt"{reg.border.y.low:.6f} {reg.border.y.high:.6f} " &
-              fmt"{reg.border.z.low:.6f} {reg.border.z.high:.6f}" & "\n"
+              fmt"{borders.x.low:.6f} {borders.x.high:.6f} " &
+              fmt"{borders.y.low:.6f} {borders.y.high:.6f} " &
+              fmt"{borders.z.low:.6f} {borders.z.high:.6f}" & "\n"
 
 proc lmp_atomcreate_cmds(sys: System): string =
   var seed = sys.atomparams.creation_seed
@@ -55,6 +84,6 @@ proc main() =
 
 
 when isMainModule:
-  error.error_info.usage = "./lmp_atom_regions <tensor|staggered|tiled> <nx> <ny> <nz> <lenx> <leny> <lenz> <region-gap> <natoms-per-region,seed,inc-seed?> <input-cuts(csv)>"
+  error.error_info.usage = "./lmp_atom_regions <tensor|staggered|tiled> <nx> <ny> <nz> <lenx> <leny> <lenz> <region-gap> <natoms-per-region,seed,inc-seed?> <dimorder,input-cuts(csv)>"
   main()
 
